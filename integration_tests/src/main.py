67901e8ebf999cfe32405101d49d8da52e601da6
Daniel Howlett <dan@dancras.co.uk>
2021-08-06 19:38:57 +0100

    Implement a crude create and fetch endpoint for lobby

--- integration_tests/src/main.py
+++ integration_tests/src/main.py
@@ -14,6 +14,34 @@ class IntegrationTests(unittest.IsolatedAsyncioTestCase):
     async def test_functionality(self):
         response = await self.session.get("http://flask_backend:5000")
         self.assertEqual(response.status, 200)
+    
+    async def test_create_lobby(self):
+        data = {
+            'host_id': 'FAKEHOST'
+        }
+        response = await self.session.post("http://flask_backend:5000/create_lobby", json=data)
+        response_data = await response.json()
+
+        self.assertEqual(response.status, 201)
+        self.assertEqual(response.headers['Location'], "http://flask_backend:5000/lobby/{}".format(response_data['id']))
+        self.assertEqual(response_data['host_id'], 'FAKEHOST')
+
+    async def set_up_lobby(self):
+        data = {
+            'host_id': 'FAKEHOST',
+        }
+        response = await self.session.post("http://flask_backend:5000/create_lobby", json=data)
+        response_data = await response.json()
+        return response_data['id']
+
+    async def test_get_lobby(self):
+        lobby_id = await self.set_up_lobby()
+        response = await self.session.get("http://flask_backend:5000/lobby/{}".format(lobby_id))
+        response_data = await response.json()
+
+        self.assertEqual(response.status, 200)
+        self.assertEqual(response_data['id'], lobby_id)
+        self.assertEqual(response_data['host_id'], 'FAKEHOST')
 
 
 if __name__ == "__main__":
--- server/src/app.py
+++ server/src/app.py
@@ -1,10 +1,46 @@
-from flask import Flask
+import json
+from flask import Flask, request
 
 app = Flask(__name__)
+
+lobby_index = 0
+lobbies = {}
+
+def next_lobby_id():
+    global lobby_index
+    lobby_index += 1
+    return lobby_index
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route('/create_lobby', methods = ['POST'])
+def create_lobby():
+    global lobbies
+    data = request.get_json()
+
+    lobby_id = next_lobby_id()
+    lobbies[lobby_id] = {
+        'id': lobby_id,
+        'host_id': data['host_id']
+    }
+
+    r = app.response_class(
+        response = json.dumps(lobbies[lobby_id]),
+        status = 201,
+        mimetype = 'application/json'
+    )
+    r.headers['Location'] = "{}lobby/{}".format(request.root_url, lobby_id)
+    return r
+
+@app.route("/lobby/<lobby_id>")
+def fetch_lobby(lobby_id):
+    return app.response_class(
+        response = json.dumps(lobbies[int(lobby_id)]),
+        mimetype = 'application/json'
+    )
+    
 
 if __name__ == "__main__":
     app.run(debug = True, host = '0.0.0.0')
