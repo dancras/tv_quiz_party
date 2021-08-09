fc5fb9e96a299d0a07adf8008137465b7dd051c1
Daniel Howlett <dan@dancras.co.uk>
2021-08-09 15:39:49 +0100

    Add join lobby endpoint supporting multi users

--- integration_tests/src/main.py
+++ integration_tests/src/main.py
@@ -16,32 +16,57 @@ class IntegrationTests(unittest.IsolatedAsyncioTestCase):
         self.assertEqual(response.status, 200)
     
     async def test_create_lobby(self):
-        data = {
-            'host_id': 'FAKEHOST'
-        }
-        response = await self.session.post("http://flask_backend:5000/create_lobby", json=data)
-        response_data = await response.json()
+        response = await self.session.post("http://flask_backend:5000/create_lobby")
 
         self.assertEqual(response.status, 201)
+
+        response_data = await response.json()
+
         self.assertEqual(response.headers['Location'], "http://flask_backend:5000/lobby/{}".format(response_data['id']))
-        self.assertEqual(response_data['host_id'], 'FAKEHOST')
+        self.assertEqual(response_data['host_id'], response.cookies['user_id'].value)
 
     async def set_up_lobby(self):
-        data = {
-            'host_id': 'FAKEHOST',
-        }
-        response = await self.session.post("http://flask_backend:5000/create_lobby", json=data)
-        response_data = await response.json()
-        return response_data['id']
+        response = await self.session.post("http://flask_backend:5000/create_lobby")
+        return await response.json()
 
     async def test_get_lobby(self):
-        lobby_id = await self.set_up_lobby()
+        lobby_data = await self.set_up_lobby()
+        lobby_id = lobby_data['id']
+        lobby_host_id = lobby_data['host_id']
+
         response = await self.session.get("http://flask_backend:5000/lobby/{}".format(lobby_id))
         response_data = await response.json()
 
         self.assertEqual(response.status, 200)
         self.assertEqual(response_data['id'], lobby_id)
-        self.assertEqual(response_data['host_id'], 'FAKEHOST')
+        self.assertEqual(response_data['host_id'], lobby_host_id)
+
+    async def test_join_lobby_fails_when_already_joined(self):
+        lobby_data = await self.set_up_lobby()
+        data = {
+            'join_code': lobby_data['join_code']
+        }
+        response = await self.session.post("http://flask_backend:5000/join_lobby", json=data)
+        self.assertEqual(response.status, 422)
+
+    async def test_join_lobby_adds_new_user(self):
+        lobby_data = await self.set_up_lobby()
+        data = {
+            'join_code': lobby_data['join_code']
+        }
+        async with aiohttp.ClientSession() as other_session:
+            response = await other_session.post("http://flask_backend:5000/join_lobby", json=data)
+            self.assertEqual(response.status, 200)
+
+            response_data = await response.json()
+            self.assertEqual(response_data['join_code'], lobby_data['join_code'])
+            self.assertIn(response.cookies['user_id'].value, response_data['users'])
+
+            lobby_response = await other_session.get(response.headers['Location'])
+            lobby_response_data = await lobby_response.json()
+            self.assertEqual(response_data['join_code'], lobby_data['join_code'])
+            self.assertIn(response.cookies['user_id'].value, lobby_response_data['users'])
+
 
 
 if __name__ == "__main__":
--- server/src/app.py
+++ server/src/app.py
@@ -1,5 +1,6 @@
 import json
-from flask import Flask, request
+import uuid
+from flask import Flask, request, g, after_this_request
 
 app = Flask(__name__)
 
@@ -11,6 +12,25 @@ def next_lobby_id():
     lobby_index += 1
     return lobby_index
 
+@app.before_request
+def manage_user_id():
+    user_id = None
+
+    try:
+        user_id = str(uuid.UUID(request.cookies.get('user_id'), version=4))
+    except (TypeError, ValueError):
+        pass
+
+    if None == user_id:
+        user_id = str(uuid.uuid4())
+
+        @after_this_request
+        def set_user_id_cookie(response):
+            response.set_cookie('user_id', user_id)
+            return response
+
+    g.user_id = user_id
+
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
@@ -18,12 +38,13 @@ def hello_world():
 @app.route('/create_lobby', methods = ['POST'])
 def create_lobby():
     global lobbies
-    data = request.get_json()
 
     lobby_id = next_lobby_id()
     lobbies[lobby_id] = {
         'id': lobby_id,
-        'host_id': data['host_id']
+        'host_id': g.user_id,
+        'join_code': lobby_id,
+        'users': [g.user_id]
     }
 
     r = app.response_class(
@@ -32,6 +53,33 @@ def create_lobby():
         mimetype = 'application/json'
     )
     r.headers['Location'] = "{}lobby/{}".format(request.root_url, lobby_id)
+    return r
+
+@app.route('/join_lobby', methods = ['POST'])
+def join_lobby():
+    global lobbies
+    data = request.get_json()
+    join_code = data['join_code']
+
+    lobby_data = lobbies[join_code]
+
+    if g.user_id in lobby_data['users']:
+        return app.response_class(
+            response = json.dumps({
+                'message': "User already in lobby"
+            }),
+            status = 422,
+            mimetype = 'application/json'
+        )
+
+    lobby_data['users'].append(g.user_id)
+
+    r = app.response_class(
+        response = json.dumps(lobby_data),
+        status = 200,
+        mimetype = 'application/json'
+    )
+    r.headers['Location'] = "{}lobby/{}".format(request.root_url, lobby_data['id'])
     return r
 
 @app.route("/lobby/<lobby_id>")
