f6cb61a22d5a8c555b174b0eb95d2d7590b888c9
Daniel Howlett <dan@dancras.co.uk>
2021-11-29 19:38:41 +0000

    Configure proxy to support websockets

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -3,12 +3,14 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app) {
   app.use(
     '/api/*',
-    createProxyMiddleware({
+    createProxyMiddleware(['/api/**'], {
       target: 'http://flask_backend:5000',
       changeOrigin: true,
       pathRewrite: {
         '^/api':''
-      }
+      },
+      ws: true,
+      logLevel: 'error'
     })
   );
 };
